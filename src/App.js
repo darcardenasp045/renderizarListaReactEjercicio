424de168d1cad2cc38ce3bf20c3d9379475ff457
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2023-03-21 15:57:52 -0500

    se agrago un nuevo archivo para practicar

--- src/App.js
+++ src/App.js
@@ -22,7 +22,15 @@ const App = () => {
     ]
   };
 
-  return <Course course={course} />;
+  if (typeof course === undefined || course === null || course === "") {
+    return <div>"no hay notas"</div>;
+  }
+
+  return (
+    <div>
+      <Course course={course} />;
+    </div>
+  );
 };
 
 export default App;
