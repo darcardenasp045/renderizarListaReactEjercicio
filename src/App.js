11e50a72bee929a53883d0f9bd09d325d6aebb4d
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2023-03-21 20:45:38 -0500

    se mejroro el contenido de course.js, haciendo que renderizara mas en app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import Course from "./Course.js";
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
-
-  if (typeof course === undefined || course === null || course === "") {
-    return <div>"no hay notas"</div>;
-  }
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: "Node.js course",
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <Course course={course} />;
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
